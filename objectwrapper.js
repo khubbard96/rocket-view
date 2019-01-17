87056f39171247729f1ff27c9d45839890614279
khubbard96 <kevlar2020@gmail.com>
2019-01-17 15:08:09 -0600

    object wrapper

---
+++ objectwrapper.js
@@ -0,0 +1,36 @@
+module.exports = class RocketPart {
+
+  /**
+  * A wrapper for Three.js 3DModels. Provides loaders and util functions,
+  * primarily for animation, motion, etc.
+  */
+
+  constructor( threeObject, options ) {
+    this.object = threeObject;
+  }
+
+  //animators
+
+  shiftUp() {
+
+
+  }
+
+  shiftDown() {
+
+
+  }
+
+  explodePart() {
+
+
+  }
+
+  //getters
+
+  getPart() {
+    return this.object;
+  }
+
+
+}
